a62e07ec71a51b8097bcfc3e27a092502f80c0e9
Nikita Ghost <nekhd@yandex.ru>
2021-06-10 16:48:43 +0300

    fix

--- compile_wallet.sh
+++ compile_wallet.sh
@@ -5,4 +5,4 @@ TON_COMPILER="/home/nikita/TON-Solidity-Compiler/build/solc/solc"
 TVM_LINKER="/home/nikita/TVM-linker/tvm_linker/target/release/tvm_linker"
 
 $TON_COMPILER SimpleWallet.sol 
-$TVM_LINKER compile SimpleWallet.code --lib /home/nikita/TON-Solidity-Compiler/lib/stdlib.tvm sol.tvm
+$TVM_LINKER compile SimpleWallet.code --lib /home/nikita/TON-Solidity-Compiler/lib/stdlib_sol.tvm
