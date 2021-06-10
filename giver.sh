5265f47fc97b7ebb8f4b2f20cc7f5f66c55ef32f
Nikita Ghost <nekhd@yandex.ru>
2021-06-10 20:16:18 +0300

    fix path

--- giver.sh
+++ giver.sh
@@ -2,8 +2,8 @@
 
 TON_CLI="/home/nikita/tonos-cli/tonos-cli"
 TON_DEAPP_ADDR="http://188.227.35.43:8080/graphql"
-TON_GIVER_KEY="/home/nikita/keys/giver/GiverV2.keys.json"
-TON_GIVER_ABI="/home/nikita/keys/giver/GiverV2.abi.json"
+TON_GIVER_KEY="/home/nikita/contracts/keys/giver/GiverV2.keys.json"
+TON_GIVER_ABI="/home/nikita/contracts/keys/giver/GiverV2.abi.json"
 TON_GIVER_ADDR="0:b5e9240fc2d2f1ff8cbb1d1dee7fb7cae155e5f6320e585fcc685698994a19a5"
 
 $TON_CLI --url $TON_DEAPP_ADDR call $TON_GIVER_ADDR sendTransaction '{"dest":"'$1'", "value":1000000000000, "bounce":false}' --abi $TON_GIVER_ABI --sign $TON_GIVER_KEY
