1002c746e3e3c5c00d6688cbd4f8bbf93c7f5dc1
Nikita Ghost <nekhd@yandex.ru>
2021-06-10 21:33:10 +0300

    fix

--- deploy_swallet.sh
+++ deploy_swallet.sh
@@ -5,5 +5,5 @@ TVC_FILE=$1
 ADDR=$2
 
 cd /home/nikita/contracts
-#sh giver.sh $2
+sh giver.sh $2
 $TON_CLI --url http://188.227.35.43:8080/graphql deploy $1 '{"owner":"0:13ed757201678bc4d0b085d26657cdb45d24924f7146c43e508f3f3296c91f19"}' --abi SimpleWallet.abi.json --sign SimpleWallet.keys.json --wc 0
