caff75310729e144242b559ee47b8745250de18f
Nikita Ghost <nekhd@yandex.ru>
2021-06-10 20:21:17 +0300

    fix

--- genaddr_swallet.sh
+++ genaddr_swallet.sh
@@ -4,5 +4,5 @@ TON_CLI="/home/nikita/tonos-cli/tonos-cli"
 TVC_FILE = $1
 
 cd /home/nikita/contracts
-$TON_CLI config --url 188.227.35.43
+$TON_CLI config --url 188.227.35.43:8080
 $TON_CLI genaddr --genkey SimpleWallet.keys.json --wc 0 $1 SimpleWallet.abi.json
