eee3eca32b18c23268259d597fa636beb1b59661
Nikita Ghost <nekhd@yandex.ru>
2021-06-12 16:44:30 +0300

    refactor

--- scripts/wallet_send.sh
+++ scripts/wallet_send.sh
@@ -2,5 +2,5 @@
 
 TON_CLI="/home/nikita/tonos-cli/tonos-cli"
 
-cd /home/nikita/contracts/simplewallet
+cd /home/nikita/contracts/wallet
 $TON_CLI call $1 sendTransaction '{"dest":"$2","value":"$3","bounce":"true"}' --abi Wallet.abi.json --sign Wallet.keys.json
