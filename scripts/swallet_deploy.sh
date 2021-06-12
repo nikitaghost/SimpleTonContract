a36a251d8989071ed5f397455641eec35357001b
Nikita Ghost <nekhd@yandex.ru>
2021-06-12 16:27:50 +0300

    refactor

--- scripts/swallet_deploy.sh
+++ scripts/swallet_deploy.sh
@@ -4,5 +4,5 @@ TON_CLI="/home/nikita/tonos-cli/tonos-cli"
 ADDR=$1
 
 cd /home/nikita/contracts/simplewallet
-sh ../scripts/giver.sh $1
+bash ../scripts/giver.sh $1
 $TON_CLI --url http://188.227.35.43:8080/ deploy SimpleWallet.tvc '{"owner":"0:13ed757201678bc4d0b085d26657cdb45d24924f7146c43e508f3f3296c91f19"}' --abi SimpleWallet.abi.json --sign SimpleWallet.keys.json --wc 0
